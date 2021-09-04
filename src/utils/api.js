bc7ec93df33dd85e5949e289c93c935699c37de0
Drew Olsen <olsenj.andrew@gmail.com>
2021-09-03 23:42:37 -0500

    Initial React Setup and some customization

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const api = () => {
+
+    // API Urls for convenience
+    // Page Content:
+    // GET https://s0nshulo19.execute-api.us-east-1.amazonaws.com/default/code-challenge
+    // Newsletter:
+    // POST https://s0nshulo19.execute-api.us-east-1.amazonaws.com/default/code-challenge
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default api
