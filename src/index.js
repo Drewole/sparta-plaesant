e8403c5cb0bad71d44ac39d1ba522f4a7116023c
Drew Olsen <olsenj.andrew@gmail.com>
2021-09-04 21:51:29 -0500

    Imported fonts to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './fonts/ACaslonPro-Regular.otf'
+import './fonts/Springville-Regular3.otf'
 
 ReactDOM.render(
   <React.StrictMode>
